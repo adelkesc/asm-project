3ea7cd2f1fc6168871abc4ce0143fa712a971fc6
Amahdya Delkescamp <256523@via.dk>
2018-11-28 16:48:02 +0100

    Made calculations and fixed logic so ultrasonic sensor should work.

--- DistanceSensor/DistanceSensor/main.asm
+++ DistanceSensor/DistanceSensor/main.asm
@@ -8,30 +8,32 @@
 ; Main project, Distance Sensor
 ; Push Button LED code moved to separate file to prevent accidents.
 
-START:
-; Setup input and output registers
-	LDI R16, 0x01
-	OUT DDRA, R16 ; port A is TRIG output
-	OUT DDRB, R16 ; port B is ECHO input
-	;OUT DDRC, R16
-
 TRIG:
 ; Outputs signal for 10 microseconds
 	LDI R16, 0x01 ; Set TRIG pin to high and output signal, pin 1 in port A contains the high signal.
+	OUT DDRA, R16 ; port A is TRIG output
+	LDI R18, 0x00
 	OUT PORTA, R16
-	; Delay goes here.
+	LDI R19, 0x35 ; Load immediate 53 into Register 19
+
+; Loop to output for 10 microseconds
+DELAY_1:
+	DEC R19
+	BRNE DELAY_1
+	OUT PORTA, R18
 	
 ECHO:
-; Receives input from signal sent by TRIG
+; Receives input from signal sent by TRIG, open for input for 20 microseconds
+	;OUT DDRB, R17 ; port B is ECHO input, dont need this because ddr is default 0
+	LDI R20, 0x6B ; Load immediate 107 into Register 20
+
+; Loop to listen for input for 20 microseconds
+DELAY_2:
+	DEC R20
+	BRNE DELAY_2
 	IN R17, PINB
-	;OUT PORTC, R17
-	; Time and distance calculations
-	; How to make these calcuations?
+	JMP TRIG
 
-	;LDI R18, 0x00 ; zero until calculations are made
-	;OUT DDRC, R18 ; nothing until calculations are made
+	; Find the time and make the distance calculation
 
-MAIN:
-	; Adjusts input from ECHO and outputs to LED (single LED or bar graph)
-	;CLR R16, R17 ; clear registers?
-	;JMP START ; Jump to START or TRIG and begin again.
+DISPLAY:
